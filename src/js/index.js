8cdc6d97b49eca2ca69b4c885ba44b4f65e87b07
Vira Smulska <vera.smulskaya@gmail.com>
2023-02-22 20:09:10 +0100

    add id to every task and action to delete of done task

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,9 @@ window.onload = () => {
 const initialUISetup = () => {
   document.getElementById("search").addEventListener("mouseover", () => addClassWithTimeout("searchLabel", "hovered", 500));
   document.getElementById("search").addEventListener("mouseout", () => removeClassWithTimeout("searchLabel", "hovered", 500));
+  document.getElementById("addTaskInput").addEventListener("keyup", (event) => vallidateField(event));
+  document.getElementById("addButton").addEventListener("click", addNewTask);
+  radioHandlerSetup();
   }
 
 const addClassWithTimeout = (id, className, timeout) => {
@@ -52,14 +55,49 @@ const enableAddTaskButton = () => {
 const addNewTask = () => {
   const inputElement = document.getElementById('addTaskInput');
   const taskWrapper = document.createElement('div');
+  const id = generateID(10);
   taskWrapper.innerHTML = getTaskTemplate(inputElement.value, id).trim();
+  document.getElementById("taskList").append(taskWrapper.firstChild);
+  inputElement.value = "";
+  disableAddTaskButton();
+  document.getElementById(id).addEventListener("click", (event) => onRadioClick(event));
 }
 
-const getTaskTemplate = (value) => {
-  return `<div class="task plainTask">
+const changeTaskStatus = (id) => {
+  const taskElement = document.getElementById(id);
+  taskElement.remove();
+  document.getElementById("doneTaskList").append(taskElement); 
+}
+
+const getTaskTemplate = (value, id) => {
+  return `<div class="task plainTask" id="${id}">
             <div class="taskName">
             <div class="radio leftElement"><img width="15" src="src/images/search.png"></div>
               <span class="taskTitle">${value}</span>
             </div>  
           </div>`
+}
+
+const generateID = (idLength) => {
+  let result = "";
+  const characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm123456789";
+  for (let i = 0; i < idLength; i++) {
+    result += characters.charAt(Math.random() * characters.length);
+  }
+  return result;
+}
+
+const onRadioClick = (event) => {
+  const taskElement = event.target.closest(".task.plainTask");
+  if (taskElement) {
+    changeTaskStatus(taskElement.getAttribute("id"));
+  }
+}
+
+const radioHandlerSetup = () => {
+  const radioButtons = document.getElementsByClassName("radio");
+  for (const radio of radioButtons) {
+    radio.removeEventListener("click", (event) => onRadioClick(event));
+    radio.addEventListener("click", (event) => onRadioClick(event));
+  }
 }
